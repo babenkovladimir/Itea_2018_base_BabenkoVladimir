d0e7d778c3b9f985ea9e34e3c6250d3f3f1b9b51
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-02-17 12:48:25 +0200

    in progress. new listners

--- app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/MainActivity.java
+++ app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.vladimirbabenko.android_base_homeworks;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupUI() {
-       // getActionBar().setDisplayHomeAsUpEnabled(true);
+        Button btGoogleSignIn = (Button) findViewById(R.id.btGoogleSignIn);
+        Button btFacebookSignIn = (Button) findViewById(R.id.btFacebookSignIn);
+        Button btLogin = (Button) findViewById(R.id.btLogin);
+        Button btTerms = (Button) findViewById(R.id.btTerms);
+        Button btSignUP = (Button) findViewById(R.id.btSignUp);
+
+        EditText etLogin = (EditText) findViewById(R.id.etLogin);
+        EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+        btFacebookSignIn.setOnClickListener(
+            view -> Toast.makeText(this, "SignIn by Facebook", Toast.LENGTH_SHORT).show());
+        btGoogleSignIn.setOnClickListener(
+            view -> Toast.makeText(this, "SignIn by Google", Toast.LENGTH_SHORT).show());
+
+        btTerms.setOnClickListener(
+            view -> Toast.makeText(this, "Show terms ", Toast.LENGTH_SHORT).show());
+        btSignUP.setOnClickListener(
+            view -> Toast.makeText(this, "SignUp is pressed", Toast.LENGTH_SHORT).show());
+
+        btLogin.setOnClickListener(view -> {
+            String password = String.valueOf(etPassword.getText());
+        });
     }
 }
