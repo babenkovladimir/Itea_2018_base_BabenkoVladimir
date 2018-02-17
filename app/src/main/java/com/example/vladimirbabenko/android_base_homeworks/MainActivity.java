e399183adb09c1796d7ff1e527739e34335049bd
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-02-17 18:55:56 +0200

    email validation; password inputType

--- app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/MainActivity.java
+++ app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,8 +37,41 @@ public class MainActivity extends AppCompatActivity {
         btSignUP.setOnClickListener(
             view -> Toast.makeText(this, "SignUp is pressed", Toast.LENGTH_SHORT).show());
 
+        /*
+        * Email validation
+        *
+        * */
+
         btLogin.setOnClickListener(view -> {
-            String password = String.valueOf(etPassword.getText());
+            String email = String.valueOf(etLogin.getText());
+            String password = String.valueOf(etLogin.getText());
+
+            if(password.length()<8) Toast.makeText(this, "password is very short", Toast.LENGTH_SHORT).show();
+
+            if (isValidEmail(email)) {
+                Toast.makeText(this, "Email is valid", Toast.LENGTH_SHORT).show();
+            } else {
+                etLogin.setError("Wrong login");
+            }
+
         });
+    }
+
+    //&& isValidPassword(password)
+
+    private boolean isValidEmail(CharSequence email) {
+        if (email == null) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        }
+    }
+    private boolean isValidPassword(CharSequence password) {
+        String regEx = "[0-9]+";
+
+        Pattern pattern = Pattern.compile(regEx);
+        Matcher matcher = pattern.matcher(password);
+
+        return matcher.matches();
     }
 }
