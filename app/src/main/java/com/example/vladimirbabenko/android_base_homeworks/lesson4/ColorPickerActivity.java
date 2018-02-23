6353abd1efa6d1861504ce6271a156a6405fe702
babenkovladimir <Babenko_mail@ukr.net>
2018-02-23 21:04:53 +0200

    colorPickerActivity.java modifying

--- app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson4/ColorPickerActivity.java
+++ app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson4/ColorPickerActivity.java
@@ -1,15 +1,34 @@
 package com.example.vladimirbabenko.android_base_homeworks.lesson4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.vladimirbabenko.android_base_homeworks.R;
 
 public class ColorPickerActivity extends AppCompatActivity {
 
+    private TextView tvRedRezult, tvGreenRezult, tvBlueRezult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_picker);
+
+        setupUI();
+
+    }
+
+    private void setupUI() {
+
+        tvRedRezult = (TextView) findViewById(R.id.tvRedRezult);
+        tvGreenRezult = (TextView) findViewById(R.id.tvGreenRezult);
+        tvBlueRezult = (TextView) findViewById(R.id.tvBlueRezult);
+
+        Button btnOpenColorPicker = findViewById(R.id.btnOpenColorPicker);
+        btnOpenColorPicker.setOnClickListener(v-> startActivity(new Intent(getApplicationContext(), ColorPickerActivity.class)));
     }
 }
