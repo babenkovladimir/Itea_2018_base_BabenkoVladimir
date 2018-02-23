5abefb02b400fc80a23a9b79db2734f15b12edfd
babenkovladimir <Babenko_mail@ukr.net>
2018-02-23 09:00:56 +0200

    init commit; add new activitis;

---
+++ app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson4/ColorPickerActivity.java
@@ -0,0 +1,15 @@
+package com.example.vladimirbabenko.android_base_homeworks.lesson4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.vladimirbabenko.android_base_homeworks.R;
+
+public class ColorPickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_picker);
+    }
+}
