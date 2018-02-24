58722945fbec0c37c580f5a04751cda31e8eb536
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-02-24 11:17:31 +0200

    colorPicker marckup;

--- app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson4/ColorPickerActivity.java
+++ app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson4/ColorPickerActivity.java
@@ -1,34 +1,42 @@
 package com.example.vladimirbabenko.android_base_homeworks.lesson4;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.example.vladimirbabenko.android_base_homeworks.R;
 
 public class ColorPickerActivity extends AppCompatActivity {
 
+    // Чтобы было красиво...
+    private TextView tvSeekBarRedRezult, tvSeekBarGreenRezult, tvSeekBarBlueRezult;
     private TextView tvRedRezult, tvGreenRezult, tvBlueRezult;
+    private SeekBar sbRed, sbGreen, sbBlue;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_picker);
 
         setupUI();
-
     }
 
     private void setupUI() {
 
-        tvRedRezult = (TextView) findViewById(R.id.tvRedRezult);
-        tvGreenRezult = (TextView) findViewById(R.id.tvGreenRezult);
-        tvBlueRezult = (TextView) findViewById(R.id.tvBlueRezult);
+        tvSeekBarRedRezult = (TextView) findViewById(R.id.tvSeekBarRedRezult);
+        tvSeekBarGreenRezult = (TextView) findViewById(R.id.tvSeekBarGreenRezult);
+        tvSeekBarBlueRezult = (TextView) findViewById(R.id.tvSeekBarBlueRezult);
 
-        Button btnOpenColorPicker = findViewById(R.id.btnOpenColorPicker);
-        btnOpenColorPicker.setOnClickListener(v-> startActivity(new Intent(getApplicationContext(), ColorPickerActivity.class)));
+        tvRedRezult = (TextView)findViewById(R.id.tvRedRezult);
+        tvGreenRezult = (TextView)findViewById(R.id.tvGreenRezult);
+        tvBlueRezult = (TextView)findViewById(R.id.tvBlueRezult);
+
+        sbRed = (SeekBar)findViewById(R.id.sbRed);
+        sbGreen = (SeekBar)findViewById(R.id.sbRed);
+        sbBlue = (SeekBar)findViewById(R.id.sbRed);
     }
 }
--- app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson4/ColorPickerRezultActivity.java
+++ app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson4/ColorPickerRezultActivity.java
@@ -1,15 +1,32 @@
 package com.example.vladimirbabenko.android_base_homeworks.lesson4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.widget.Button;
+import android.widget.TextView;
 import com.example.vladimirbabenko.android_base_homeworks.R;
 
 public class ColorPickerRezultActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private TextView tvRedRezult, tvGreenRezult, tvBlueRezult;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_picker_rezult);
+
+        setupUI();
+    }
+
+    private void setupUI() {
+
+        tvRedRezult = (TextView) findViewById(R.id.tvRedRezult);
+        tvGreenRezult = (TextView) findViewById(R.id.tvGreenRezult);
+        tvBlueRezult = (TextView) findViewById(R.id.tvBlueRezult);
+
+        Button btnOpenColorPicker = findViewById(R.id.btnOpenColorPicker);
+        btnOpenColorPicker.setOnClickListener(
+            v -> startActivity(new Intent(getApplicationContext(), ColorPickerActivity.class)));
     }
 }
