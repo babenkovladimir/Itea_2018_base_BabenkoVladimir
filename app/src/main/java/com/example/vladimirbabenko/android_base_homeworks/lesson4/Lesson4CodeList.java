d02244c4db15556ccd775b165ad6db79a450c969
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-02-24 21:06:39 +0200

    create intereface for constants

---
+++ app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson4/Lesson4CodeList.java
@@ -0,0 +1,12 @@
+package com.example.vladimirbabenko.android_base_homeworks.lesson4;
+
+/**
+ * Created by VladimirBabenko on 2/24/2018.
+ */
+
+public interface Lesson4CodeList {
+    public int COLOR_PICKER_REQUEST_CODE = 1;
+    public String COLOR_OBJECT_KEY = "colorObjectKey";
+    public String COLOR_BUNDLE_KEY = "colorBundleKey";
+
+}
