f0bc69348180e0121c44c418f956aaae998ec285
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-03-09 23:58:15 +0200

    lombok

--- app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson7_practice/entity/BookEntity.java
+++ app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson7_practice/entity/BookEntity.java
@@ -4,30 +4,23 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 public class BookEntity implements Parcelable{
 
-    private @Getter @Setter String nameofBook;
+    private @Getter @Setter String nameOfBook;
     private @Getter @Setter String author;
     private @Getter @Setter String imageUrl;
     private @Getter @Setter float rate;
     private @Getter @Setter String description;
 
-    //public BookEntity(String nameofBook, String author, String imageUrl, float rate,
-    //    String description) {
-    //    this.nameofBook = nameofBook;
-    //    this.author = author;
-    //    this.imageUrl = imageUrl;
-    //    this.rate = rate;
-    //    this.description = description;
-    //}
-
     protected BookEntity(Parcel in) {
-        nameofBook = in.readString();
+        nameOfBook = in.readString();
         author = in.readString();
         imageUrl = in.readString();
         rate = in.readFloat();
@@ -44,32 +37,12 @@ public class BookEntity implements Parcelable{
         }
     };
 
-    //public String getNameofBook() {
-    //    return nameofBook;
-    //}
-    //
-    //public String getAuthor() {
-    //    return author;
-    //}
-    //
-    //public String getImageUrl() {
-    //    return imageUrl;
-    //}
-    //
-    //public float getRate() {
-    //    return rate;
-    //}
-    //
-    //public String getDescription() {
-    //    return description;
-    //}
-
     @Override public int describeContents() {
         return 0;
     }
 
     @Override public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(nameofBook);
+        dest.writeString(nameOfBook);
         dest.writeString(author);
         dest.writeString(imageUrl);
         dest.writeFloat(rate);
