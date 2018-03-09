7eed9839f97ebc3f6cc710b05c35bc63e55f1f8f
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-03-09 23:58:36 +0200

    lombok

--- app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson7_practice/entity/BookEntity.java
+++ app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson7_practice/entity/BookEntity.java
@@ -8,10 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-public class BookEntity implements Parcelable{
+@AllArgsConstructor @NoArgsConstructor @ToString public class BookEntity implements Parcelable {
 
     private @Getter @Setter String nameOfBook;
     private @Getter @Setter String author;
