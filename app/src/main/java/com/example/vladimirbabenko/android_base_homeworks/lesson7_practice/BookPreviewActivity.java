ec1ef35eff35ccdd7e9454b32d9f942c87ff8850
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-03-10 13:30:57 +0200

    final;

--- app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson7_practice/BookPreviewActivity.java
+++ app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson7_practice/BookPreviewActivity.java
@@ -1,10 +1,8 @@
 package com.example.vladimirbabenko.android_base_homeworks.lesson7_practice;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
