3234a2ca130a15ca5c956665f012426330f48817
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-03-26 21:21:49 +0300

    add singleton initialization;

--- app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson7_practice/data/DataManager.java
+++ app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson7_practice/data/DataManager.java
@@ -1,15 +1,22 @@
 package com.example.vladimirbabenko.android_base_homeworks.lesson7_practice.data;
 
 import android.content.Context;
-import android.support.v4.content.ContextCompat;
 import com.example.vladimirbabenko.android_base_homeworks.lesson7_practice.entity.BookEntity;
 import java.util.List;
 
 public class DataManager {
     private RepositoryMockBooks mRepositoryMockBooks;
     private PreferencesHelper mPreferencesHelper;
+    private static DataManager instance;
 
-    public DataManager(Context context) {
+    public static synchronized DataManager getInstance(Context context) {
+        if (instance == null) {
+            instance = new DataManager(context);
+        }
+        return instance;
+    }
+
+    private DataManager(Context context) {
         mRepositoryMockBooks = new RepositoryMockBooks();
         mPreferencesHelper = new PreferencesHelper(context);
     }
