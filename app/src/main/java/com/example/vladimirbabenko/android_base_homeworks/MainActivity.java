2751117f4db8f278d3cfcbe45aec07c645130197
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-03-29 19:26:44 +0300

    initial commit;

--- app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/MainActivity.java
+++ app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import com.example.vladimirbabenko.android_base_homeworks.lesson10.Lesson11Activity;
 import com.example.vladimirbabenko.android_base_homeworks.lesson12.DialogFragmentActivity;
 import com.example.vladimirbabenko.android_base_homeworks.lesson13.Lesson13;
+import com.example.vladimirbabenko.android_base_homeworks.lesson14.Lesson14Activity;
 import com.example.vladimirbabenko.android_base_homeworks.lesson4ClassMaterial.IntentExampleActivity;
 import com.example.vladimirbabenko.android_base_homeworks.lesson4HomeWork.ColorPickerRezultActivity;
 import com.example.vladimirbabenko.android_base_homeworks.lesson5ClassMaterial.ArrayAdapterExampleActivity;
@@ -22,7 +23,7 @@ import com.example.vladimirbabenko.android_base_homeworks.lesson9.AlertDialogAct
 public class MainActivity extends AppCompatActivity {
 
     private Button btShowSignInActivity, btShowOtherTasks, btCustomUserViewActivity, btDynamicGui,
-        btColorPicker, btImplicitCalls, btArrayAdapterAndLifecycle, bCustomArrayAdapter, btLesson7Practice, btLesson9Practice, btLesson10Practice,btLesson12DialogFragment, btLesson13;
+        btColorPicker, btImplicitCalls, btArrayAdapterAndLifecycle, bCustomArrayAdapter, btLesson7Practice, btLesson9Practice, btLesson10Practice,btLesson12DialogFragment, btLesson13, btLesson14;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         btLesson10Practice = (Button) findViewById(R.id.btLesson10Practice);
         btLesson12DialogFragment = (Button)findViewById(R.id.btLesson12DialogFragment);
         btLesson13 = (Button)findViewById(R.id.btLesson13);
+        btLesson14 = (Button)findViewById(R.id.btLesson14);
 
         btShowSignInActivity.setOnClickListener(
             view -> startActivity(new Intent(this, MainActivityHomeWorkScreen.class)));
@@ -81,5 +83,6 @@ public class MainActivity extends AppCompatActivity {
         btLesson12DialogFragment.setOnClickListener(view-> startActivity(new Intent(this, DialogFragmentActivity.class)));
 
         btLesson13.setOnClickListener(view-> startActivity(new Intent(this, Lesson13.class)));
+        btLesson14.setOnClickListener(view -> startActivity(new Intent(this, Lesson14Activity.class)));
     }
 }
---
+++ app/src/main/java/com/example/vladimirbabenko/android_base_homeworks/lesson14/Lesson14Activity.java
@@ -0,0 +1,13 @@
+package com.example.vladimirbabenko.android_base_homeworks.lesson14;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.vladimirbabenko.android_base_homeworks.R;
+
+public class Lesson14Activity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson14);
+    }
+}
